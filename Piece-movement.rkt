9cfdaf100183886499de28a3ef69d74b6bbce723
Michael Bertucci <mbertucci1@users.noreply.github.com>
2017-04-22 03:35:51 -0400

    Update Piece-movement.rkt
    -Completed implementation of piece capture mechanics. User is forced to make a capture on their turn if they have one available, and if they have multiple options the game prompts them to choose one. This can happen as many times as needed until no more captures can be made that turn.
    -Moved get-red-pieces and get-black-pieces here from game-loop.rkt in order to assist in some of the computations made by functions in this file.

--- Piece-movement.rkt
+++ Piece-movement.rkt
@@ -11,6 +11,10 @@
 (define (move-piece tile1 tile2)
   (cond ((or (equal? tile1 '()) (equal? tile2 '()))
          (displayln "Illegal move: invalid coordinates") 'illegal-move)
+        ((and (not (null? (pieces-that-can-capture (tile1 'get-piece))))
+              (or (not (member tile1 (pieces-that-can-capture (tile1 'get-piece))))
+                  (not (member tile2 (capturable-tiles tile1)))))
+         (displayln "You must make an available capture!") 'illegal-move)
         ((equal? (tile1 'get-piece) 'red)
          (cond ((equal? (tile2 'get-piece) 'none)
                 (begin
@@ -30,8 +34,7 @@
         (else (displayln "Illegal move: no piece at start") 'illegal-move)))
 
 (define (capture tile1 tile2)
-  (define tile3 (get-board-element (+ (tile2 'get-row) (- (tile2 'get-row) (tile1 'get-row)))
-                                   (+ (tile2 'get-column) (- (tile2 'get-column) (tile1 'get-column)))))
+  (define tile3 (jump-destination-tile tile1 tile2))
   (cond ((equal? tile3 '()) "Invalid co-ordinates -- capture")
         ((equal? (tile3 'get-piece) 'none)
          (cond ((equal? (tile1 'get-piece) 'red)
@@ -39,32 +42,71 @@
                   ((tile1 'set-piece) 'none)
                   ((tile2 'set-piece) 'none)
                   ((tile3 'set-piece) 'red)
-                  ;'piece-captured))
-                  (continue-capture tile3 'red)))
+                  (continue-capture tile3)))
                ((equal? (tile1 'get-piece) 'black)
                 (begin
                   ((tile1 'set-piece) 'none)
                   ((tile2 'set-piece) 'none)
                   ((tile3 'set-piece) 'black)
-                  ;'piece-captured))
-                  (continue-capture tile3 'black)))
+                  (continue-capture tile3)))
                (else "no piece at start -- capture")))
         (else "destination is occupied -- capture")))
 
-(define (continue-capture tile color)
-  (let ((options (if (equal? color 'red)
-                     (filter (lambda (t) (< (t 'get-row) (tile 'get-row))) (find-adjacent-opponents tile color)) ;tiles in row above with opponent piece
-                     (filter (lambda (t) (> (t 'get-row) (tile 'get-row))) (find-adjacent-opponents tile color))))) ;in row below
+(define (continue-capture tile)
+  (let ((options (capturable-tiles tile)))
     (cond ((null? options) 'capture-done)
           ((= 1 (length options)) (capture tile (car options)))
           (else (capture tile (choose-next-capture options))))))
 
+(define (pieces-that-can-capture color)
+  (filter (lambda (tile) (not (null? (capturable-tiles tile))))
+          (cond ((equal? color 'red) (get-red-pieces))
+                ((equal? color 'black) (get-black-pieces))
+                (else '()))))
+
+(define (display-tile-ID tile)
+  (if (null? tile)
+      (void)
+      (begin 
+        (display (integer->char (+ (tile 'get-column) 64)))
+        (display (tile 'get-row))
+        (display " "))))
+
+(define (list-numbers start end)
+  (if (> start end)
+      '()
+      (cons start (list-numbers (+ start 1) end))))
+
+(define (display-enumerated-tile-list lst)
+  (map (lambda (tile num) (begin (display "(")
+                                 (display num)
+                                 (display ")")
+                                 (display-tile-ID tile)))
+       lst
+       (list-numbers 1 (length lst))))
+
 (define (choose-next-capture lst)
   (begin (displayln (draw-board))
-         (display "Choose next piece to capture: ")
-         (map (lambda (t) (display (integer->char (+ (t 'get-column) 64))) (display (t 'get-row)) (display " ")) lst)
+         (display "Choose next piece to capture (enter the number): ")
+         (display-enumerated-tile-list lst)
          (newline)
-         (car lst))) ;change this - for now just selects the car instead of asking user
+         (list-ref lst (- (user-selection 1 (length lst)) 1))))
+
+(define (user-selection min max)
+  (let ((input (read)))
+    (if (and (integer? input)
+             (<= input max)
+             (>= input min))
+        input
+        (begin (displayln "Invalid selection. Please enter a valid number: ")
+               (user-selection min max)))))
+
+;; Get the tile that would be the landing spot of a jump made by a piece on
+;; tile1 over an opposing piece on tile2. This function may return a null value
+;; if the destination would be outside the bounds of the board.
+(define (jump-destination-tile tile1 tile2)
+  (get-board-element (+ (tile2 'get-row) (- (tile2 'get-row) (tile1 'get-row)))
+                     (+ (tile2 'get-column) (- (tile2 'get-column) (tile1 'get-column)))))
 
 (define (find-adjacent-opponents tile color)
   (filter (lambda (t) (and (not (null? t))
@@ -75,7 +117,45 @@
           (list (get-board-element (- (tile 'get-row) 1) (+ (tile 'get-column) 1))
                 (get-board-element (- (tile 'get-row) 1) (- (tile 'get-column) 1))
                 (get-board-element (+ (tile 'get-row) 1) (+ (tile 'get-column) 1))
-                (get-board-element (+ (tile 'get-row) 1) (- (tile 'get-column) 1))))) 
+                (get-board-element (+ (tile 'get-row) 1) (- (tile 'get-column) 1)))))
+
+;; For a given tile, figure out if the piece on it can capture any opponent
+;; pieces, and return a list of those tiles that have pieces it can capture.
+;; Does not take into account restrictions on the direction a piece can move.
+(define (capture-options tile)
+  (if (not (or (equal? (tile 'get-piece) 'red)
+               (equal? (tile 'get-piece) 'black)))
+      ;no capture options for a tile with no piece on it
+      '()
+      ;else, generate a list of all the adjacent tiles that have opponent pieces
+      ; and filter only the ones that have an empty space beyond them
+      (filter (lambda (t) (and (not (null? (jump-destination-tile tile t)))
+                               (equal? ((jump-destination-tile tile t) 'get-piece) 'none)))
+              (find-adjacent-opponents tile (tile 'get-piece)))))
+
+;; Restricts the results of capture-options based on how the piece on the tile
+;; is allowed to move.
+(define (capturable-tiles tile)
+  (cond ;((tile 'king?) (capture-options tile))
+    ((equal? (tile 'get-piece) 'red)
+     (filter (lambda (t) (< (t 'get-row) (tile 'get-row))) (capture-options tile))) ;only tiles in row above
+    ((equal? (tile 'get-piece) 'black)
+     (filter (lambda (t) (> (t 'get-row) (tile 'get-row))) (capture-options tile))) ;only tiles in row below
+    (else '())))
+
+(define (get-black-pieces)
+  (define (get-black-pieces-recurse row)
+    (if (= row max-row) '()
+    (append (filter (lambda (tile) (equal? (tile 'get-piece) 'black)) (get-row row board))
+          (get-black-pieces-recurse (add1 row)))))
+  (get-black-pieces-recurse 0));generates list of all black tiles
+
+(define (get-red-pieces)
+  (define (get-red-pieces-recurse row)
+    (if (= row max-row) '()
+    (append (filter (lambda (tile) (equal? (tile 'get-piece) 'red)) (get-row row board))
+          (get-red-pieces-recurse (add1 row)))))
+  (get-red-pieces-recurse 0));generates list of all red tiles
 
 (define (move-in-direction tile direction)
   (cond ((equal? direction 'north)
